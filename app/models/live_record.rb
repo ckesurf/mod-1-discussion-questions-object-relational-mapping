bca925d36c0b63e59c65bbca915614842d7c1f5a
Chris Erlendson <ckesurf@gmail.com>
2022-11-01 17:50:39 -0400

    solution

---
+++ app/models/live_record.rb
@@ -0,0 +1,19 @@
+class LiveRecord
+  # This is the dog or cat's name, as opposed to the Class name! which is used later
+  attr_accessor :name
+
+  def self.all
+    # self.name.downcase + "s"
+    sql = "SELECT * FROM #{self.name.downcase + 's'}"
+    results = DB[:conn].execute(sql);
+  end
+end
+
+class Dog < LiveRecord
+end
+
+class Cat < LiveRecord
+end
+
+# Dog.all #=> 'SELECT * FROM dogs'
+# Cat.all #=> 'SELECT * FROM cats'
